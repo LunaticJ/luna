18296d5dd88ea00cc3dc72084fc7b8af899b8930
riku <jasonlu1994@hotmail.com>
2020-12-15 22:29:28 +0800

    vert.x mongo verticle init

---
+++ data-service/src/main/java/com/lunaticj/luna/data/constants/DataSchemaApiConstants.java
@@ -0,0 +1,14 @@
+package com.lunaticj.luna.data.constants;
+
+/**
+ * data schema api uri constants
+ *
+ * @author kuro
+ * @create 2020-12-14
+ **/
+public class DataSchemaApiConstants {
+  public static final String DATA_SCHEMA_GET = "/data-schema/:id";
+  public static final String DATA_SCHEMA_LIST_GET = "/data-schema";
+  public static final String DATA_SCHEMA_CREATE = "/data-schema/:id";
+  public static final String DATA_SCHEMA_UPDATE = "/data-schema/:id";
+}
