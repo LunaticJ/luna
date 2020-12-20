368d708437cf28a3d490f4adb6e392ff1b821c4e
riku <jasonlu1994@hotmail.com>
2020-12-20 21:01:05 +0800

    init simple-01

---
+++ simple-01/src/test/java/com/lunaticj/luna/simple01/Simple01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lunaticj.luna.simple01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Simple01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
