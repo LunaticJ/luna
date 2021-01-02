bfdd3d3386d3d1d63257504a206ab5f7cddbba22
riku <jasonlu1994@hotmail.com>
2021-01-02 23:31:30 +0800

    vertex-restful-data data-verticle init

---
+++ vertex-restful-data/src/main/java/com/lunaticj/luna/data/verticle/DataVerticle.java
@@ -0,0 +1,10 @@
+package com.lunaticj.luna.data.verticle;
+
+import io.vertx.core.AbstractVerticle;
+
+/**
+ * @author kuro
+ * @create 2021-01-02
+ **/
+public class DataVerticle extends AbstractVerticle {
+}
