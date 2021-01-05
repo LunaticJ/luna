b02cc30a05ce7e03c0a68359649220789ff8ae0f
riku <jasonlu1994@hotmail.com>
2021-01-05 23:00:47 +0800

    vertex-mnemosyne data verticle start init

--- vertex-mnemosyne/src/main/java/com/lunaticj/luna/mnemosyne/verticle/DataVerticle.java
+++ vertex-mnemosyne/src/main/java/com/lunaticj/luna/mnemosyne/verticle/DataVerticle.java
@@ -1,10 +1,15 @@
 package com.lunaticj.luna.mnemosyne.verticle;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
 
 /**
  * @author kuro
  * @create 2021-01-02
  **/
 public class DataVerticle extends AbstractVerticle {
+  @Override
+  public void start(Promise<Void> startPromise) throws Exception {
+    super.start(startPromise);
+  }
 }
