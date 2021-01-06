74694ca664eb0323d1963f176a85439867f2762a
riku <jasonlu1994@hotmail.com>
2021-01-06 22:41:22 +0800

    mongo client init

--- vertex-mnemosyne/src/main/java/com/lunaticj/luna/mnemosyne/verticle/DataVerticle.java
+++ vertex-mnemosyne/src/main/java/com/lunaticj/luna/mnemosyne/verticle/DataVerticle.java
@@ -2,14 +2,22 @@ package com.lunaticj.luna.mnemosyne.verticle;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.mongo.MongoClient;
+
 
 /**
  * @author kuro
  * @create 2021-01-02
  **/
 public class DataVerticle extends AbstractVerticle {
+
+  private MongoClient mongoClient = null;
+
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
-    super.start(startPromise);
+    JsonObject dbConfig = config();
+    mongoClient = MongoClient.createShared(vertx, dbConfig);
+    startPromise.complete();
   }
 }
