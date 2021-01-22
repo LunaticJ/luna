64f4010f70e42542578241c662fe5aa1ab733c1f
riku <jasonlu1994@hotmail.com>
2021-01-22 23:13:42 +0800

    springboot themis service init

---
+++ saber-themis/src/main/java/com/lunaticj/luna/themis/SaberThemisApplication.java
@@ -0,0 +1,13 @@
+package com.lunaticj.luna.themis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaberThemisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaberThemisApplication.class, args);
+    }
+
+}
---
+++ saber-themis/src/test/java/com/lunaticj/luna/themis/SaberThemisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lunaticj.luna.themis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaberThemisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
