adf00889283abf0995845349d687689282c89124
riku <jasonlu1994@hotmail.com>
2021-02-12 17:23:35 +0800

    flutter in action demo 1

--- flutter_demo/startup_namer/lib/main.dart
+++ flutter_demo/startup_namer/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Startup Name Generator',
       theme: new ThemeData(
-        primaryColor: Colors.white
+        primarySwatch: Colors.grey
       ),
       home: RandomWords(),
     );
