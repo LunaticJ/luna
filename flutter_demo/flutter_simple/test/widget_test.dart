f74e3f57a58d568bc3fb75a6134b724f56a89ebd
riku <jasonlu1994@hotmail.com>
2021-02-13 22:55:16 +0800

    flutter simple demo for view

--- flutter_demo/flutter_simple/test/widget_test.dart
+++ flutter_demo/flutter_simple/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_simple/simple1_text.dart';
+import 'package:flutter_simple/simple01_text.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
